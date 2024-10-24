29be879edc3ddb5dedf56fd7f874537e82b00f25
amicevski <amicevski@thirdpxl.com>
2024-10-24 20:01:42 +0200

    Initial Commit

---
+++ PlaceIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaceIt
+//
+//  Created by Aleksandar Micevski on 24.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
